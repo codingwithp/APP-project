0e6841b3917cb4c58a6a718389000e327bbc0cc6
Pragathi <pragathi.is22@bmsce.ac.in>
2025-01-02 23:27:46 +0530

    Finally done  baby

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import subprocess
+
 
 # Initialize Flask app
 app = Flask(__name__)
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
 def home():
-    # Serve the HTML page
     return render_template("home.html")
 
-
+# Route for Student Registration
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 
 @app.route("/mark", methods=["POST"])
 def mark_attendance():
@@ -24,9 +31,10 @@ def mark_attendance():
             return jsonify({"message": f"Error in recognition script: {result.stderr}"}), 500
     except Exception as e:
         return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+    
+@app.route('/table')
+def table():
+    return render_template('table.html')
 
-
-
-# Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
--- normal.py
+++
@@ -1,8 +0,0 @@
-import recognition
-
-if __name__ == "__main__":
-    try:
-        result = recognition.mark_attendance()
-        print("Attendance marked:", result)
-    except Exception as e:
-        print("Error:", e)
